e62350a4b160990b2fc85926946e87fe35c7edcc
safwan <safwaniftekher@gmail.com>
2024-03-21 16:31:11 +0600

    Refactor

---
+++ functions/api.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require("path");
+const serverless = require('serverless-http');
+
+const app = express(); 
+
+app.use(express.static(path.join(__dirname, "uploads")));
+
+require("../startup/routes")(app);
+require("../startup/db")();
+require("../startup/db")();
+
+const router = express.Router();
+
+app.use('/.netlify/functions/server', router);
+
+module.exports.handler = serverless(app);
--- startup/routes.js
+++ startup/routes.js
@@ -2,12 +2,10 @@ const express = require("express");
 const animal = require("../routes/animal");
 const category = require("../routes/category");
 const error = require("../middleware/error");
-const router = express.Router();
 
 module.exports = function (app) {
   app.use(express.json());
   app.use("/api/animal", animal);
   app.use("/api/category", category);
   app.use(error);
-  app.use('/.netlify/functions/server', router);
 };
