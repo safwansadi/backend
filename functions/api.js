486b275574525e66b6e4473ea6957bec5d2b82c7
safwan <safwaniftekher@gmail.com>
2024-03-21 16:51:11 +0600

    Refactor

--- functions/api.js
+++ functions/api.js
@@ -1,17 +1,25 @@
 const express = require('express');
 const path = require("path");
 const serverless = require('serverless-http');
+const animal = require("../routes/animal");
+const category = require("../routes/category");
+const error = require("../middleware/error");
 
 const app = express(); 
+const router = express.Router();
 
 app.use(express.static(path.join(__dirname, "uploads")));
 
-require("../startup/routes")(app);
-require("../startup/db")();
-require("../startup/db")();
+router.get("/",(req, res)=>{
+  res.send("app is running...")
+});
 
-const router = express.Router();
-
+// Mount router at the specified base path
 app.use('/.netlify/functions/api', router);
+
+app.use(express.json());
+app.use("/animal", animal);
+app.use("/category", category);
+app.use(error);
 
 module.exports.handler = serverless(app);
