778401b5c6aa2896a91d0e03605b255906ecd701
safwan <safwaniftekher@gmail.com>
2024-03-21 15:41:58 +0600

    deploy

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const path = require("path");
 const {PORT} = require('./config');
+const serverless = require('serverless-http');
 
 const app = express();
+
 app.use(express.static(path.join(__dirname, "uploads")));
 
 require("./startup/routes")(app);
 require("./startup/db")();
 require("./startup/validation")();
 
+module.exports.handler = serverless(app);
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
